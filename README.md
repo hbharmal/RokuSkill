407e92f875b0c506e2a3473d47c75e9951cf5b41
Hussain Bharmal <hbharmal@utexas.edu>
2018-11-06 22:47:31 -0600

    removed ignored directory

--- README.md
+++
@@ -1,2 +0,0 @@
-# RokuSkill
-I'm creating an alexa app that works with a Roku TV. I want my application to be flexible enough to handle several edge cases. 
--- RokuLamda/index.js
+++
@@ -1,9 +0,0 @@
-const Alexa = require('ask-sdk-core');
-const server_info = require('./server_info');
-var http = require("http");
-
-// App_ID associated with the alexa skill
-const APP_ID = server_info.appId;
-
-const skillBuilder = Alexa.SkillBuilders.custom();
-
